e71f590ab68428a8037329aeebcb9430969fe33f
fujix129 <fujix.sasekikun.co.jp>
2018-09-14 15:23:14 +0900

    メニューのレイアウト調整

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="Menu">
+
     <div class="MenuContainer">
       <h2>プロフィール</h2>
       <p>きなこさんの画像入れる予定</p>
       <!-- APIから取得するようにしたい -->
       <dl>
-        <dt>名前</dt><dd>fujix</dd>
+        <dt>なまえ</dt><dd>fujix(hyoufk)</dd>
         <dt>twitter</dt><dd><a href="https://twitter.com/hyoufk">@hyoufk</a></dd>
         <dt>github</dt><dd><a href="https://github.com/fujix129">@fujix129</a></dd>
       </dl>
+      <p>まだまだペーペーのプログラマ見習い。</p>
+      <p>飼い猫のきなこさんのお世話の為に生きてる。</p>
     </div>
+
     <div class="MenuContainer">
-      <h2>カテゴリ</h2>
+      <h2>最近の。</h2>
+      <p>直近の記事</p>
     </div>
+
     <div class="MenuContainer">
-      <h2>月毎の記事</h2>
+      <h2>月毎の。</h2>
     </div>
-    <div class="MenuContainer Twitter">
+
+    <div class="MenuContainer">
+      <h2>カテゴリ毎の。</h2>
+    </div>
+
+    <div class="MenuContainer">
       <Twitter />
     </div>
+
   </div>
 </template>
 
@@ -49,16 +61,18 @@ dd {
   float:left;
   width:70%;
 }
+p {
+  font-size: 12px;
+}
 .Menu {
   float: right;
   width: 25%;
 }
 .MenuContainer {
   width: 80%;
-  margin: 10% 10% 30% 10%;
-  padding-top: 10%;
-}
-.Twitter {
-  height: 50%;
+  margin: 10% 10% 10% 10%;
+  padding-top: 20px;
+  border: solid 1px #cccccc;
+  border-radius: 10px;
 }
 </style>
