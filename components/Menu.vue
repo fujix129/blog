abfeb001947a8ca24b5b8124dee93b9373fa0cab
fujix129 <fujix.sasekikun.co.jp>
2018-09-14 14:02:50 +0900

    画面脇のメニューを仮追加

---
+++ components/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="Menu">
+    <div class="MenuContainer">
+      <h2>プロフィール</h2>
+      <p>きなこさんの画像入れる予定</p>
+      <!-- APIから取得するようにしたい -->
+      <dl>
+        <dt>名前</dt><dd>fujix</dd>
+        <dt>twitter</dt><dd><a href="https://twitter.com/hyoufk">@hyoufk</a></dd>
+        <dt>github</dt><dd><a href="https://github.com/fujix129">@fujix129</a></dd>
+      </dl>
+    </div>
+    <div class="MenuContainer">
+      <h2>カテゴリ</h2>
+    </div>
+    <div class="MenuContainer">
+      <h2>月毎の記事</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.Menu {
+  float: right;
+  width: 20%;
+}
+.MenuContainer {
+  width: 80%;
+  margin: 10% 10% 30% 10%;
+  padding-top: 10%;
+}
+h2 {
+  border-top: solid 1px #cccccc;
+  border-bottom: solid 1px #cccccc;
+}
+dl {
+  width:100%;
+}
+dt {
+  float:left;
+  clear:left;
+  width:30%;
+}
+dd {
+  float:left;
+  width:70%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header />
+    <Menu />
     <nuxt/>
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import Menu from '~/components/Menu.vue'
 
 export default {
   components: {
-    Header
+    Header,
+    Menu
   }
 }
 </script>
@@ -18,7 +21,6 @@ export default {
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -31,34 +33,5 @@ html {
 *, *:before, *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
